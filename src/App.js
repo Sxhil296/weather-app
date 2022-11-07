5e119e599fbf5574a6e9b294a16adc937a2c4ec3
Sahil <leviack629@gmail.com>
2022-11-07 23:07:39 +0530

    errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
 function App() {
+
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=62a4b1e49b868663d831f6b8ca9df326`
+
   return (
     <div className="App">
-    <h1>hello</h1>
+    <div className="container">
+      <div className="top">
+        <div className="location">
+          <p>Delhi</p>
+        </div>
+        <div className="temp">
+          <h1>65F</h1>
+        </div>
+        <div className="description">
+          <p>Clouds</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          <p>60F</p>
+        </div>
+        <div className="humidity">
+          <p>20%</p>
+        </div>
+        <div className="wind">
+          <p>12 KMH</p>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
